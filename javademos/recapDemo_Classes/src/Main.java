827ea64462ad53615deaa08b5cccef27deddfbf9
Nurullah Diri <nurullahdiri875@gmail.com>
2022-10-02 01:53:15 +0300

    Add files via upload

---
+++ javademos/recapDemo_Classes/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DortIslem dortIslem = new DortIslem();
+         int result = dortIslem.topla(1,3);
+        System.out.println(result);
+    }
+}
