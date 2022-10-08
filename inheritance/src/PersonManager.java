d3ea5c348a1b9b80aafd1bce3126e8687366c661
Nurullah Diri <nurullahdiri875@gmail.com>
2022-10-08 20:12:47 +0300

    Delete inheritance directory

--- inheritance/src/PersonManager.java
+++
@@ -1,9 +0,0 @@
-public class PersonManager {
-    public void list() {
-        System.out.println("müsteri listelendi");
-    }
-
-    public void add() {
-        System.out.println("müsteri eklendi");
-    }
-}
