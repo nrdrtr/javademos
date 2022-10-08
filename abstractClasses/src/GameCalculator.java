77173fa0624767ef651bc873a9096f8f0c399fb7
Nurullah Diri <nurullahdiri875@gmail.com>
2022-10-08 20:09:39 +0300

    Delete abstractClasses directory

--- abstractClasses/src/GameCalculator.java
+++
@@ -1,8 +0,0 @@
-public abstract class GameCalculator {
-
-    public abstract void hesapla();//bu operasyonun default'u olmasın herkes override etsin ve kendi kurallarını içersin
-
-    public final void gameOver() {// bu sınıfı  miras alan sınıflar bu operasyonu değiştiremez
-        System.out.println(" Oyun bitti");
-    }
-}
